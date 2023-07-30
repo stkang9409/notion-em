5cc2b1954968242c7e52cbfb11da065493a37e83
mk.kang <stkang9409@gmail.com>
2023-07-31 08:27:58 +0900

    asdf

---
+++ unsexy/articles.js
@@ -0,0 +1,11 @@
+function generatePreviewComponent(article) {
+  getLinkPreview("https://www.youtube.com/watch?v=MejbOFk7H6c").then((data) =>
+    console.debug(data)
+  );
+  //   return (
+  //     <div>
+  //       <h1>{article.title}</h1>
+  //       <p>{article.body}</p>
+  //     </div>
+  //   );
+}
--- unsexy/calendar.js
+++ unsexy/calendar.js
@@ -1,13 +1,7 @@
-const getKoreanDate = (date) => {
-  const timezoneOffset = date.getTimezoneOffset() * 60 * 1000;
-  return new Date(date.getTime() + timezoneOffset + 9 * 60 * 60 * 1000);
-};
 // get startDate from query string
 const queryStringObj = new URLSearchParams(location.search);
-const startDate = queryStringObj.get("startDate")
-  ? getKoreanDate(new Date(queryStringObj.get("startDate")))
-  : new Date(document.getElementById("month").dataset.start);
-const today = getKoreanDate(new Date());
+const startDate = new Date(queryStringObj.get("startDate"));
+const today = new Date("2023-08-18");
 const year = today.getFullYear();
 const month = today.getMonth() + 1;
 const date = today.getDate();
@@ -16,14 +10,12 @@ const dayNames = ["일", "월", "화", "수", "목", "금", "토"];
 
 function requireReportNum(start_date, today) {
   let days = Math.floor((today - start_date) / 1000 / 60 / 60 / 24) + 1;
-  if (29 <= days && days <= 35) {
-    days = 28;
-  } else if (days > 35) {
-    days = days - 7;
+  const originDays = days;
+  days -= Math.floor(days / 7) * 2;
+  // 토요일 일요일 제외
+  if (originDays > 5 && originDays % 7 === 6) {
+    days -= 1;
   }
-
-  days -= Math.floor(days / 7);
-
   return days;
 }
 
@@ -31,4 +23,5 @@ function requireReportNum(start_date, today) {
 document.getElementById("month").innerText = month;
 document.getElementById("date").innerText = date;
 document.getElementById("day").innerText = dayNames[day];
+console.log("!!!", startDate, today);
 document.getElementById("count").innerText = requireReportNum(startDate, today);
